eead341a65eb904ec8c727302c86fa311770b412
elvin <elvinyeka@gmail.com>
2020-10-21 21:54:26 +0400

    mesage update

--- .history/CLEANBLOG/__init___20201021215252.py
+++ .history/CLEANBLOG/__init___20201021215252.py
@@ -9,9 +9,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_'] = False
 app.config['MAIL_USERNAME'] = 'elvinyeka@gmail.com'
-app.config['MAIL_PASSWORD'] = 'yekayev1011'
+app.config['MAIL_PASSWORD'] = 'example'
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
